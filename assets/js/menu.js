8e3fe8f643da0e165cbc59b7356a0b7a8dfbe2b4
Tanis Pérez Nartallo <tanisperez@gmail.com>
2024-08-13 19:52:18 +0200

    Simplify menu javascript

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,21 +2,11 @@
 
 const menuTrigger = document.querySelector(".menu-trigger");
 const menu = document.querySelector(".menu");
-const mobileQuery = getComputedStyle(document.body).getPropertyValue(
-    "--phoneWidth"
-);
-const isMobile = () => window.matchMedia(mobileQuery).matches;
-const isMobileMenu = () => {
-    menuTrigger && menuTrigger.classList.toggle("hidden", !isMobile());
-    menu && menu.classList.toggle("hidden", isMobile());
-};
 
-isMobileMenu();
-
-menuTrigger &&
-    menuTrigger.addEventListener(
-        "click",
-        () => menu && menu.classList.toggle("hidden")
-    );
-
-window.addEventListener("resize", isMobileMenu);
+menuTrigger.addEventListener("click", () => {
+    if (menu.computedStyleMap().get('display').value === 'none') {
+        menu.style['display'] = 'block';
+    } else {
+        menu.style.removeProperty('display');
+    }
+});
